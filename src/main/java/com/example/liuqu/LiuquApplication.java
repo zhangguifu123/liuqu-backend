d425a1718909d244ef9a4f825a3fe48403ac9532
“张桂福” <904824669@qq.com>
2023-07-08 22:34:10 +1000

    Initial commit

---
+++ src/main/java/com/example/liuqu/LiuquApplication.java
@@ -0,0 +1,13 @@
+package com.example.liuqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuquApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiuquApplication.class, args);
+	}
+
+}
