4e721c9bb91cc4160118d55d1af7dfefcfdb994b
“张桂福” <904824669@qq.com>
2023-08-28 18:26:32 +1000

    登录注册

---
+++ src/main/java/com/example/liuqu/Model/User.java
@@ -0,0 +1,76 @@
+package com.example.liuqu.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer uid;
+    private String nickname;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String token;
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+}
---
+++ src/test/java/com/example/liuqu/LiuquApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liuqu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuquApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
