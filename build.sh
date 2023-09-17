9f89e985a1534c952e922c10996abd6195a69b40
“张桂福” <904824669@qq.com>
2023-09-17 12:50:44 +1000

    upload jar + delete properties and build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 打jar包
-./gradlew buildTodoCoderJar
-# 构建docker镜像
-docker build -t springboot/springboot-liuqu:v1.0.0 .
-# 运行镜像
-docker run --name=springboot-liuqu -d -p 8080:8080 springboot/springboot-liuqu:v1.0.0
-
