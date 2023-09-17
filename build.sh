60d3c222f573a14d28dbf6002b6fd1ced5fdb8c1
“张桂福” <904824669@qq.com>
2023-09-17 12:38:50 +1000

    test EC2 connect to RDS

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 # 构建docker镜像
 docker build -t springboot/springboot-liuqu:v1.0.0 .
 # 运行镜像
-docker run --name=springboot-liuqu-d -p 8080:8080 springboot/springboot-liuqu:v1.0.0
+docker run --name=springboot-liuqu -d -p 8080:8080 springboot/springboot-liuqu:v1.0.0
 
