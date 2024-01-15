cc4b07e467f8251c16fd37adcc0dac7797065444
Ego1437 <ego@omatesydney.com>
2024-01-15 20:32:25 +1100

    【ID1000036】活动板块——修改配置文件测试redis

--- build.sh
+++ build.sh
@@ -24,7 +24,8 @@ fi
 docker build -t springboot/springboot-liuqu:v1.0.0 -f ./docker/springboot/Dockerfile .
 
 # Run the container by image
-#docker run --name=springboot-liuqu -d --restart=always --add-host host.docker.internal:host-gateway -p 8083:8080 springboot/springboot-liuqu:v1.0.0
-docker run --name=springboot-liuqu -d --add-host host.docker.internal:host-gateway -p 8083:8080 springboot/springboot-liuqu:v1.0.0
+#docker run --name=springboot-liuqu -d --add-host host.docker.internal:host-gateway -p 8083:8080 springboot/springboot-liuqu:v1.0.0
+docker run --name=springboot-liuqu -d --add-host host.docker.internal:host-gateway -p 8083:8080 -e SPRING_REDIS_HOST=clustercfg.liuqu.kyx9nh.apse2.cache.amazonaws.com -e SPRING_REDIS_PORT=6379 springboot/springboot-liuqu:v1.0.0
+
 # Run Liquibase migrations with the correct properties file
 docker run --rm -it -v "$(pwd)/src/main/resources/db/changelog:/liquibase/changelog" liquibase/liquibase-mysql update --defaultsFile=$LIQUIBASE_DEFAULTS_FILE
