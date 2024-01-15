cfc7d42eec2dc27c3ff71fb7d44fde3b25b484c2
Ego1437 <ego@omatesydney.com>
2024-01-15 20:44:16 +1100

    【ID1000036】活动板块——修改配置文件测试redis

--- build.sh
+++ build.sh
@@ -19,13 +19,24 @@ else
   echo "Usage: $0 [prod|test]"
   exit 1
 fi
+echo "Creating Docker network..."
+docker network create spring-redis-net
+
+# 启动 Redis 容器
+echo "Starting Redis container..."
+docker run --name redis-server -d --network spring-redis-net redis
+
+# 构建和运行 Spring Boot 容器
+echo "Building and starting Spring Boot container..."
+docker build -t springboot/springboot-liuqu:v1.0.0 -f ./docker/springboot/Dockerfile .
+docker run --name=springboot-liuqu -d --network=spring-redis-net -p 8083:8080 -e SPRING_REDIS_HOST=redis-server -e SPRING_REDIS_PORT=6379 springboot/springboot-liuqu:v1.0.0
 
 # build docker image
-docker build -t springboot/springboot-liuqu:v1.0.0 -f ./docker/springboot/Dockerfile .
+#docker build -t springboot/springboot-liuqu:v1.0.0 -f ./docker/springboot/Dockerfile .
 
 # Run the container by image
 #docker run --name=springboot-liuqu -d --add-host host.docker.internal:host-gateway -p 8083:8080 springboot/springboot-liuqu:v1.0.0
-docker run --name=springboot-liuqu -d --add-host host.docker.internal:host-gateway -p 8083:8080 -e SPRING_REDIS_HOST=clustercfg.liuqu.kyx9nh.apse2.cache.amazonaws.com -e SPRING_REDIS_PORT=6379 springboot/springboot-liuqu:v1.0.0
+#docker run --name=springboot-liuqu -d --add-host host.docker.internal:host-gateway -p 8083:8080 -e SPRING_REDIS_HOST=clustercfg.liuqu.kyx9nh.apse2.cache.amazonaws.com -e SPRING_REDIS_PORT=6379 springboot/springboot-liuqu:v1.0.0
 
 # Run Liquibase migrations with the correct properties file
 docker run --rm -it -v "$(pwd)/src/main/resources/db/changelog:/liquibase/changelog" liquibase/liquibase-mysql update --defaultsFile=$LIQUIBASE_DEFAULTS_FILE
--- rebuild.sh
+++ rebuild.sh
@@ -3,14 +3,26 @@
 # 默认环境为test，除非通过命令行传递了参数
 ENVIRONMENT=${1:-test}
 
-# Stop the existing container
-docker stop springboot-liuqu
+# 停止并移除现有的 Spring Boot 容器
+docker stop springboot-liuqu || true
+docker rm springboot-liuqu || true
 
-# Remove the stopped container
-docker rm springboot-liuqu
+# 移除现有的 Spring Boot Docker 镜像
+docker rmi springboot/springboot-liuqu:v1.0.0 || true
 
-# Remove the existing image
-docker rmi springboot/springboot-liuqu:v1.0.0
+# 创建或检查 Docker 网络
+docker network inspect spring-redis-net >/dev/null 2>&1 || \
+    docker network create spring-redis-net
 
-# Call build.sh with the environment specified
+# 检查 Redis 容器是否已经运行
+if [ ! "$(docker ps -q -f name=redis-server)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=redis-server)" ]; then
+        # 如果容器已停止，删除它
+        docker rm redis-server
+    fi
+    # 运行 Redis 容器
+    docker run --name redis-server -d --network spring-redis-net redis
+fi
+
+# 调用 build.sh 脚本来重新构建和启动 Spring Boot 应用
 ./build.sh $ENVIRONMENT
