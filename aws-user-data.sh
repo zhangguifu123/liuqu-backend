e6d27689e192d06358813cb8e2e3abc65a06c1b6
classmateada <classmateada@gmail.com>
2024-01-16 19:02:04 +1100

    Replace docker commands

--- aws-user-data.sh
+++ aws-user-data.sh
@@ -7,11 +7,19 @@ yum install docker -y
 systemctl start docker.service
 systemctl enable docker.service
 
-# Login and pull the latest docker image from GitHub
-# !!! Replace USERNAME with your GitHub username !!!
-# !!! Replace YOUR_GITHUB_ACCESS_TOKEN with your GitHub access token to image registry !!!
-echo YOUR_GITHUB_ACCESS_TOKEN | sudo docker login ghcr.io -u USERNAME --password-stdin
-sudo docker pull ghcr.io/Ego1437/liuqu-backend:latest
+sudo yum -y install docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+
+# !!! REPLACE THIS WITH YOUR ECR USER CREDENTIALS !!!
+export AWS_ACCESS_KEY_ID='YOUR_AWS_ACCESS_KEY'
+export AWS_SECRET_ACCESS_KEY='YOUR_AWS_ACCESS_SECRET'
+export AWS_DEFAULT_REGION=ap-southeast-2
+
+# Configure AWS user for ECR
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" && aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" && aws configure set region "$AWS_DEFAULT_REGION"
+# Login to our private ECR
+aws ecr get-login-password --region ap-southeast-2 | sudo docker login --username AWS --password-stdin 992382689127.dkr.ecr.ap-southeast-2.amazonaws.com
 
 # Launch backend instance via docker run
 sudo docker run --name=springboot-liuqu -d --restart=always --add-host host.docker.internal:host-gateway -p 8083:8080 ghcr.io/Ego1437/liuqu-backend:latest
