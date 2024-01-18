56ddcabff8b48230adc7f2fbc41ee6e818c58e6b
Ego1437 <ego@omatesydney.com>
2024-01-18 16:42:51 +1100

    【ID1000026】消息列表【一级页面】——消息通知的查、改

--- src/main/java/com/omate/liuqu/model/Notification.java
+++ src/main/java/com/omate/liuqu/model/Notification.java
@@ -1,40 +1,156 @@
 package com.omate.liuqu.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Table(name = "Notification")
+@Table(name = "notifications")
 public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long notificationId;
 
-//    @Column(length = 50, nullable = false)
-//    private String userId;
-
-    @Column(length = 30, nullable = false)
-    private String userEmail;
-
-//    @Column(length = 20, nullable = false)
-//    private String activityId;
+    @Column(nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startTime;
 
     @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    private Date eventTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
 
-    @Column(length = 100)
-    private String promotion;
+    @Column(length = 244)
+    private String message;
+
+    private Integer status;
 
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    @JsonIgnore
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "activity_id", referencedColumnName = "activity_id", nullable = false)
+    @JsonIgnore
     private Activity activity;
 
+    @ManyToOne
+    @JoinColumn(name = "partner_id", referencedColumnName = "partner_id", nullable = false)
+    @JsonIgnore
+    private Partner partner;
+
+    @Column(name = "partner_id", updatable = false, insertable = false)
+    private Long partnerId;
+
+    @Column(name = "activity_id", updatable = false, insertable = false)
+    private Long activityId;
+
+    @Column(name = "user_id", updatable = false, insertable = false)
+    private Long userId;
     // getter和setter方法
+
+    public Long getPartnerId() {
+        return partnerId;
+    }
+
+    public void setPartnerId(Long partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(Long notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Partner getPartner() {
+        return partner;
+    }
+
+    public void setPartner(Partner partner) {
+        this.partner = partner;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        startTime = now;
+        updateTime = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updateTime = LocalDateTime.now();
+    }
 }
 
