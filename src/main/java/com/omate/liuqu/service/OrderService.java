20f5ba34ad0ce51d55dd450dc633aa0b55a87946
Ego1437 <ego@omatesydney.com>
2024-01-26 22:04:15 +1100

    【ID1000018】用户【个人信息】—— 日志打点

--- src/main/java/com/omate/liuqu/service/OrderService.java
+++ src/main/java/com/omate/liuqu/service/OrderService.java
@@ -89,7 +89,7 @@ public class OrderService {
     }
     private String generateNonceStr() {
         String candidateChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        int length = 10 + new Random().nextInt(23);
+        int length = 15;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
             sb.append(candidateChars.charAt(new Random().nextInt(candidateChars.length())));
