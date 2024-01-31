2e2be1b98e7dc4ce3a984b0320c94765275237e3
Ego1437 <ego@omatesydney.com>
2024-01-31 16:46:16 +1100

    【ID1000024】【图片】—— 图片上传

--- src/main/java/com/omate/liuqu/model/Result.java
+++ src/main/java/com/omate/liuqu/model/Result.java
@@ -37,6 +37,29 @@ public class Result implements Serializable {
     private String message;
     private Object data;
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 
     public void setResultSuccess(int code) {
         this.message = resultMessage.get(code);
